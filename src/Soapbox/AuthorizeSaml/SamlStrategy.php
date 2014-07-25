692b05d48c1939ce6e9383bb9eaff07ac1e5610c
Jaspaul Bola <jaspaul.b@gmail.com>
2014-07-25 11:17:52 -0400

    Extending our abstraction

--- src/Soapbox/AuthorizeSaml/SamlStrategy.php
+++ src/Soapbox/AuthorizeSaml/SamlStrategy.php
@@ -7,4 +7,61 @@ use SoapBox\Authorize\Strategies\SingleSignOnStrategy;
 
 class SamlStrategy extends SingleSignOnStrategy {
 
+	/**
+	 * The url to redirect the user to after they have granted permissions on
+	 * SAML.
+	 *
+	 * @var string
+	 */
+	private $redirectUrl = '';
+
+	/**
+	 * Initializes the Saml Strategy for logging in.
+	 *
+	 * @param array $settings ...
+	 */
+	public function __construct($settings = array()) {
+		//Do something ...
+	}
+
+	/**
+	 * Used to authenticate our user through one of the various methods.
+	 *
+	 * @param array parameters ...
+	 *
+	 * @throws AuthenticationException If the provided parameters do not
+	 *	successfully authenticate.
+	 *
+	 * @return User A mixed array representing the authenticated user.
+	 */
+	public function login($parameters = array()) {
+		//Do something ...
+	}
+
+	/**
+	 * Used to retrieve the user from the remote SAML server.
+	 *
+	 * @param array parameters The parameters required to authenticate against
+	 *	this strategy.
+	 *
+	 * @throws AuthenticationException If the provided parameters do not
+	 *	successfully authenticate.
+	 *
+	 * @return User A mixed array representing the authenticated user.
+	 */
+	public function getUser($parameters = array()) {
+		//Do something ...
+	}
+
+	/**
+	 * Used to handle tasks after login. This could include retrieving our users
+	 * token after a successful authentication.
+	 *
+	 * @return array Mixed array of the tokens and other components that
+	 *	validate our user.
+	 */
+	public function endpoint() {
+		//Do something ...
+	}
+
 }
