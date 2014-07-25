508f030e998fe4db99ceda8c8d964e24c585c432
Jaspaul Bola <jaspaul.b@gmail.com>
2014-07-25 15:21:08 -0400

    Add basic structure for SAML integration with SimpleSAML

--- src/Soapbox/AuthorizeSaml/SamlStrategy.php
+++ src/Soapbox/AuthorizeSaml/SamlStrategy.php
@@ -16,12 +16,57 @@ class SamlStrategy extends SingleSignOnStrategy {
 	private $redirectUrl = '';
 
 	/**
+	 * The url to redirect the user if there was an error authenticating with
+	 * SAML.
+	 *
+	 * @var string
+	 */
+	private $errorUrl = '';
+
+	/**
+	 * The SAML instance
+	 *
+	 * @var SimpleSAML_Auth_Simple
+	 */
+	private $saml;
+
+	/**
+	 * Returns the default if the value is not set.
+	 *
+	 * @param $value mixed The value you wish to validate.
+	 * @param $default mixed The value you wish to get if value is not set
+	 *
+	 * @return mixed
+	 */
+	private function getValueOrDefault($value, $default) {
+		if (isset($value)) {
+			return $value;
+		}
+		return $default;
+	}
+
+	/**
 	 * Initializes the Saml Strategy for logging in.
 	 *
 	 * @param array $settings ...
 	 */
 	public function __construct($settings = array()) {
-		//Do something ...
+		if (!isset($settings['id']) ||
+			!isset($settings['redirect_url']) ||
+			!isset($settings['error_url'])) {
+			throw new \Exception(
+				'An id, redirect_url, error_url are requried to use SAML login'
+			);
+		}
+
+		//Export settings for config.php, authsources.php, and saml20-idp-remote
+		dd($config);
+
+		$this->saml = new SimpleSAML_Auth_Simple($settings['id']);
+		$this->saml->requireAuth();
+
+		$this->redirectUrl = $settings['redirect_url'];
+		$this->errorUrl = $settings['error_url'];
 	}
 
 	/**
@@ -35,7 +80,14 @@ class SamlStrategy extends SingleSignOnStrategy {
 	 * @return User A mixed array representing the authenticated user.
 	 */
 	public function login($parameters = array()) {
-		//Do something ...
+		if (!$this->saml->isAuthenticated()) {
+			$this->saml->login([
+				'ErrorURL' => $this->errorUrl,
+				'ReturnTo' => $this->redirectUrl
+			]);
+		}
+
+		return $this->getUser($parameters);
 	}
 
 	/**
@@ -50,7 +102,30 @@ class SamlStrategy extends SingleSignOnStrategy {
 	 * @return User A mixed array representing the authenticated user.
 	 */
 	public function getUser($parameters = array()) {
-		//Do something ...
+		if (!$this->saml->isAuthenticated()) {
+			return $this->login($parameters);
+		}
+
+		try {
+			$attributes = $this->saml->getAttributes();
+		} catch (\Exception $ex) {
+			throw new AuthenticationException();
+		}
+
+		$user = new User;
+
+		if (isset($parameters['email'])) {
+			$user->email = $this->getValueOrDefault($attributes[$parameters['email']][0], '');
+		} else {
+			$name_id = $as->getAuthData('saml:sp:NameID');
+			$user->email = $name_id['Value'];
+		}
+
+		$user->id = $this->getValueOrDefault($attributes[$paramters['id']][0], '');
+		$user->firstname = $this->getValueOrDefault($attributes[$parameters['firstname']][0], '');
+		$user->lastname = $this->getValueOrDefault($attributes[$parameters['lastname']][0], '');
+
+		return $user;
 	}
 
 	/**
@@ -61,7 +136,7 @@ class SamlStrategy extends SingleSignOnStrategy {
 	 *	validate our user.
 	 */
 	public function endpoint() {
-		//Do something ...
+		return $this->login();
 	}
 
 }
