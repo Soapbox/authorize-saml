bfb8c16aae454cce91d79fa497bddf3f2f2c70c4
Jaspaul Bola <jaspaul.b@gmail.com>
2014-07-29 18:00:10 -0400

    Add dynamic metadata generation

--- src/Soapbox/AuthorizeSaml/Libraries/SimpleSAMLphp/metadata/saml20-idp-remote.php
+++ src/Soapbox/AuthorizeSaml/Libraries/SimpleSAMLphp/metadata/saml20-idp-remote.php
@@ -1,19 +1,3 @@
 <?php
 
-// include_once(__DIR__ . '/../../../../../../../../../vendor/soapbox/authorize/src/SoapBox/Authorize/Strategy.php');
-// include_once(__DIR__ . '/../../../../../../../../../vendor/soapbox/authorize/src/SoapBox/Authorize/Strategies/SingleSignOnStrategy.php');
-// include_once(__DIR__ . '/../../../SamlStrategy.php');
-
-//$metadata = SoapBox\AuthorizeSaml\SamlStrategy::$metadata;
-
-$metadata['https://openidp.feide.no'] = array(
-	'name' => array(
-		'en' => 'Feide OpenIdP - guest users',
-		'no' => 'Feide Gjestebrukere',
-	),
-	'description'          => 'Here you can login with your account on Feide RnD OpenID. If you do not already have an account on this identity provider, you can create a new one by following the create new account link and follow the instructions.',
-
-	'SingleSignOnService'  => 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
-	'SingleLogoutService'  => 'https://openidp.feide.no/simplesaml/saml2/idp/SingleLogoutService.php',
-	'certFingerprint'      => 'c9ed4dfb07caf13fc21e0fec1572047eb8a7a4cb'
-);
+$metadata = SoapBox\AuthorizeSaml\SamlStrategy::$metadata;
