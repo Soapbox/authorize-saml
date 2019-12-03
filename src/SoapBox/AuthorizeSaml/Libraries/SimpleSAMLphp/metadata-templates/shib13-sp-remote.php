e9edb1d585c7354d0e0a505d26982ef101fce6e9
Graham McCarthy <graham@soapboxhq.com>
2019-12-03 14:11:30 -0800

    SimpleSAML php version from 1.7.x to 1.17.8 (#9)
    * upgrade simplesaml version from 1.7.x to 1.17.8
    
    * remove php 5.3
    
    * Update .travis.yml
    
    our servers run 5.6.. so... let's just test on that platform.
    
    * try this?
    
    * nope.

--- src/SoapBox/AuthorizeSaml/Libraries/SimpleSAMLphp/metadata-templates/shib13-sp-remote.php
+++ src/SoapBox/AuthorizeSaml/Libraries/SimpleSAMLphp/metadata-templates/shib13-sp-remote.php
@@ -1,16 +1,15 @@
 <?php
 /**
- * SAML 1.1 remote SP metadata for simpleSAMLphp.
+ * SAML 1.1 remote SP metadata for SimpleSAMLphp.
  *
- * See: https://rnd.feide.no/content/sp-remote-metadata-reference
+ * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-sp-remote
  */
 
 /*
  * This is just an example:
  */
-$metadata['https://sp.shiblab.feide.no'] = array(
-	'AssertionConsumerService' => 'http://sp.shiblab.feide.no/Shibboleth.sso/SAML/POST',
-	'audience'                 => 'urn:mace:feide:shiblab',
-	'base64attributes'         => FALSE,
-);
-
+$metadata['https://sp.shiblab.feide.no'] = [
+    'AssertionConsumerService' => 'http://sp.shiblab.feide.no/Shibboleth.sso/SAML/POST',
+    'audience' => 'urn:mace:feide:shiblab',
+    'base64attributes' => false,
+];
--- src/SoapBox/AuthorizeSaml/Libraries/SimpleSAMLphp/modules/riak/config-templates/module_riak.php
+++ src/SoapBox/AuthorizeSaml/Libraries/SimpleSAMLphp/modules/riak/config-templates/module_riak.php
@@ -2,16 +2,15 @@
 /*
  * The configuration of the riak Store module
  *
- * $Id$
  */
 
-$config = array (
-	/*
-	 * This module has the following config options and defaults.
-	 *
-	 * 'path' => 'riak-php-client/riak.php',
-	 * 'host' => 'localhost',
-	 * 'port' => 8098,
-	 * 'bucket' => 'SimpleSAMLphp',
-	 */
-);
+$config = [
+    /*
+     * This module has the following config options and defaults.
+     *
+     * 'path' => 'riak-php-client/riak.php',
+     * 'host' => 'localhost',
+     * 'port' => 8098,
+     * 'bucket' => 'SimpleSAMLphp',
+     */
+];
